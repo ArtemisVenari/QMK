cc993dad3e2861cb4b5c5d1700f6190e93eedcc3
ArtemisVenari <artemisvenari@rubox.is>
2022-03-25 16:46:09 +0100

    Add: key string Macro

--- keyboards/dz60/keymaps/artemis_alter_tsang/keymap.c
+++ keyboards/dz60/keymaps/artemis_alter_tsang/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#include "keystring.h"
+
 enum custom_keycodes {
     SIGN = SAFE_RANGE,
 };
@@ -10,7 +12,8 @@ enum keyboard_layers {
   SUPER      // Super Layer
 };
 
-bool process_record_user(unint16_t keycode, keyrecord_t *record);
+bool process_record_user(uint16_t keycode, keyrecord_t *record);
+void post_process_record_user(uint16_t keycode, keyrecord_t *record);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_60_tsangan(
@@ -22,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
     [SHIFT] = LAYOUT_60_tsangan(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,         KC_DEL,
-		_______,          KC_Q, KC_UP,   KC_ENT, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      RGB_TOG,
-		_______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,       _______,
+		_______,          KC_Q, KC_UP,   KC_ENT, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX,      RGB_TOG,
+		_______,          KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT,  XXXXXXX, XXXXXXX,       _______,
 		_______,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,          _______, _______,
 		_______, XXXXXXX, _______,                            DM_REC1,                                               _______, XXXXXXX, _______
 	),
@@ -40,12 +43,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SIGN:
             if(record->event.pressed) {
-                SEND_STRING("Mit freundlichen Grüßen \n");
-            } else {
-
+                SEND_STRING("GRUS");
             }
             break;
     }
 
     return true;
+}
+
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SIGN:
+            break;
+
+    }
 }
---
+++ keyboards/dz60/keymaps/artemis_alter_tsang/keystring.h
@@ -0,0 +1,10 @@
+//
+// Created by artemisvenari on 25.03.2022.
+//
+
+#ifndef DZ60_KEYSTRING_H
+#define DZ60_KEYSTRING_H
+
+#define GRUS    "Mit freundlichen Grüßen \n Sebastian Limberg"
+
+#endif //DZ60_KEYSTRING_H
